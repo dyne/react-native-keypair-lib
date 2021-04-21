9a576947969eee46a7a5f48d82e8086e53d26eba
pasquale <pasfranc@gmail.com>
2021-04-21 14:16:12 +0200

    Removed external libs

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 // main index.js
-
-import {QUESTION_FOLDER, QUESTION_FILE_PREPEND} from "react-native-dotenv";
-import * as RNFS from "react-native-fs";
-const DEFAULT_CLIENT_SIDE_CONTRACT = __dirname + "/zencode/Keypair-Creation-Client-Side.zen";
 const REGULAR_EXPRESSION = /\W/gi;
 const EMPTY_STRING  = "";
 const DEFAULT_USER  = "user";
-const DEFAULT_LOCALE = "en_GB";
-const DEFAULT_QUESTION_FOLDER = "/props/";
 
 const ReactNativeKeypairLib = {
-    getSafetyQuestions: function(userLocale){
-        const locale = userLocale ? userLocale : DEFAULT_LOCALE;
-        const questionFolder = QUESTION_FOLDER ? QUESTION_FOLDER : DEFAULT_QUESTION_FOLDER;
-        const prependFileName = QUESTION_FILE_PREPEND ? QUESTION_FILE_PREPEND : "questions-";
-        const propertiesFileName = questionFolder + prependFileName + locale +".json";
-        const defaultPropertiesFileName = __dirname + "/props/questions-en_GB.json";
-        return readJSONFromFile(propertiesFileName, defaultPropertiesFileName);
-    },
     sanitizeAnswers: function(answers){
         for (const key in answers) {
             if (answers[key]) {
@@ -28,10 +14,7 @@ const ReactNativeKeypairLib = {
           }
           return answers;
     },
-    recoveryKeypair: async function(answers, PBKDF, username){
-        const CLIENT_SIDE_CONTRACT = readStringFromFile(
-            DEFAULT_CLIENT_SIDE_CONTRACT
-          );
+    recoveryKeypair: async function(clientSideContractText, answers, PBKDF, username){
         const user = username ? username : DEFAULT_USER;
         const keys = {
             userChallenges: answers,
@@ -40,31 +23,17 @@ const ReactNativeKeypairLib = {
           };
           const data  = {};
         
-          const execution = await zencode_exec(CLIENT_SIDE_CONTRACT, {
+          const execution = await zencode_exec(clientSideContractText, {
             data: JSON.stringify(data),
             keys: JSON.stringify(keys),
           });
         
           return JSON.parse(execution.result);
     },
-    verifyAnswers: async function(answers, PBKDF, username, userPublicKey){
-        const execution = await recoveryKeypair(answers, PBKDF, username);
+    verifyAnswers: async function(clientSideContractText, answers, PBKDF, username, userPublicKey){
+        const execution = await recoveryKeypair(clientSideContractText, answers, PBKDF, username);
         return userPublicKey === execution[username].keypair.public_key;
     }
 }
-
-const readJSONFromFile = async function (filePath, defaultFilePath) {
-    return JSON.parse(readStringFromFile(filePath, defaultFilePath));
-};
-  
-const readStringFromFile = async function (filePath, defaultFilePath) {
-    try {
-        const file = await RNFS.readFile(filePath);
-        return file.toString();
-    } catch (err) {
-        const file = await RNFS.readFile(defaultFilePath);
-        return file.toString();
-    }
-};
 
 export default ReactNativeKeypairLib;

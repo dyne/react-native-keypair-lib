7a3c511b9eea1e78a9ed20d4c99de59bf9e1565a
pasquale <pasfranc@gmail.com>
2021-04-22 13:48:04 +0200

    Attempt to make a react native lib

---
+++ lib/index.d.ts
@@ -0,0 +1,1 @@
+export { recoveryKeypair, verifyAnswers, sanitizeAnswers, } from "./clientSide";
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export {
+  recoveryKeypair,
+  verifyAnswers,
+  sanitizeAnswers,
+} from "./clientSide";

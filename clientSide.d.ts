0cda4bc4775fbf184d34c89216c0d33f6d30422e
pasquale <pasfranc@gmail.com>
2021-04-23 11:33:56 +0200

    Changed structure

--- clientSide.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function sanitizeAnswers(answers: any): any;
-export declare function recoveryKeypair(clientSideContractText: string, answers: any, PBKDF: string): Promise<any>;
-export declare function verifyAnswers(clientSideContractText: string, answers: any, PBKDF: string, userPublicKey: string): Promise<boolean>;
--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sanitizeAnswers = exports.verifyAnswers = exports.recoveryKeypair = void 0;
-var clientSide_1 = require("./clientSide");
-Object.defineProperty(exports, "recoveryKeypair", { enumerable: true, get: function () { return clientSide_1.recoveryKeypair; } });
-Object.defineProperty(exports, "verifyAnswers", { enumerable: true, get: function () { return clientSide_1.verifyAnswers; } });
-Object.defineProperty(exports, "sanitizeAnswers", { enumerable: true, get: function () { return clientSide_1.sanitizeAnswers; } });
